535e36675c7ecd2b41e2c40c359d41cfc40ed054
ShaikSameera-design <shaiksameera2624@gmail.com>
2025-01-10 23:21:23 +0000

    CommentsAndStatements

---
+++ Javascript/8_01_25.js
@@ -0,0 +1,37 @@
+// to check whether given number is multiple of 5 or not
+/*let num=prompt("enter number:");
+if(num%5==0)
+{
+    console.log(num,"is multiple of 5");
+}
+else {
+    console.log(num,"is  not multiple  of 5");
+}
+*/
+// giving grades to a student
+let marks=prompt("enter total marks:");
+if(marks>=91 && marks<=100)
+{
+    console.log("EX");
+}
+else if(marks>=81 && marks<=90)
+{
+    console.log("A");
+}
+else if(marks>=71 && marks<=80)
+{
+    console.log("B");
+}
+else if(marks>=81 && marks<=90)
+{
+    console.log("C");
+}  
+else if(marks>=81 && marks<=90)
+{
+    console.log("D");
+} 
+else 
+{
+    console.log("FAIL");
+    
+}
