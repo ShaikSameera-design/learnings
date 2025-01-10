d8de0aaff674a32471558360978d9014d68dd3b5
ShaikSameera-design <shaiksameera2624@gmail.com>
2025-01-10 23:19:44 +0000

    BasicsAndVariables

---
+++ Javascript/7_01_25.js
@@ -0,0 +1,15 @@
+const product= {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+const profile={
+    username: "sameera",
+    isFollow: false,
+    followers: 92,
+    following: 110,
+}
+console.log(product);
+console.log(profile);
+console.log(typeof(profile));
